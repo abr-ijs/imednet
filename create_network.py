677ae5c437a4a180c0d48f55b90300e77ffe95ec
Marcel Salmič <salmic.marcel@gmail.com>
2018-01-12 01:49:48 +0100

    Learned for images from 3500-4500

--- create_network.py
+++ create_network.py
@@ -36,13 +36,13 @@ momentum = 0.2
 decay = [1e-9,1e-6]
 bunch = 1
 oneDigidOnly = False
-#Good data: 0-100, 200-2500, 5000-5100
+#Good data: 0-100, 200-3500, 5000-5100
 #Bad data: 100-200
 use_data = np.arange(0,100)
-use_data = np.append(use_data,np.arange(200,2500))
+use_data = np.append(use_data,np.arange(200,3500))
 use_data = np.append(use_data,np.arange(5000,5100))
-data = 3500
-s_data = 2500
+data = 4500
+s_data = 3500
 artificial_samples = 0
 digit = 0
 
