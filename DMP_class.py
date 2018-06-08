backward corrected, addet dt and minus to gradient computation, problems with not changing test and validation results

DMP_class.py
@@ -183,7 +183,7 @@ class DMP(object):
         """
 
 
-        time_steps = int(np.round(np.max(self.tau)/self.dt))
+        time_steps = int(np.round(np.max(self.tau)/self.dt))+1
 
         dt = self.tau/time_steps
 

DMP_layer.py
@@ -90,8 +90,8 @@ class DMP_integrator(Function):
 
 
 
-        X = integrate(parameters,w, inputs_np[:,range(0,int(parameters[0].item()))].view(int(parameters[0].item())*2,), torch.zeros(2*int(parameters[0].item())).cuda(),
-                      inputs_np[:,range(int(parameters[0].item()),int(parameters[0].item())*2)].view(int(parameters[0].item())*2,), 3)
+        X = integrate(parameters,w, inputs_np[:,range(0,int(parameters[0].item()))].view(int(parameters[0].item())*inputs.shape[0],), torch.zeros(inputs.shape[0]*int(parameters[0].item())).cuda(),
+                      inputs_np[:,range(int(parameters[0].item()),int(parameters[0].item())*2)].view(int(parameters[0].item())*inputs.shape[0],), 3)
 
 
         return inputs.new(X)
@@ -131,13 +131,10 @@ class DMP_integrator(Function):
                 point_grads[t,j + 2] = sum(grad[:,  1] * grad_outputs[t*int(parameters[0].item())+j, :])'''
 
 
+        point_grads = torch.mm(grad_outputs,grad).view(-1,2,27).transpose(2,1).contiguous().view(1,-1,54).squeeze()
 
 
-
-        k = torch.mm(grad_outputs,grad).view(2,-1,27).transpose(2,1).contiguous().view(2,-1,54).squeeze()
-
-
-        point_grads = k*scale
+        point_grads = -2*point_grads*scale*parameters[3].item()
         #import pdb;
         #pdb.set_trace()
 
@@ -211,7 +208,7 @@ class createDMPparam():
         self.sigma2 = torch.from_numpy(sigma2).float()
         self.tau = tau
         self.dt = dt
-        self.time_steps = int(np.round(self.tau / self.dt))
+        self.time_steps = int(np.round(self.tau / self.dt))+1
         self.y0 = [0]
         self.dy0 = np.zeros(Dof)
         self.Dof = Dof
@@ -229,7 +226,7 @@ class createDMPparam():
 
         self.scale_tensor = scale_tensor
         # precomputation
-        grad = torch.zeros((300, 27))
+        grad = torch.zeros((301, 27))
 
         self.data = {'time_steps':self.time_steps,'c':self.c,'sigma2':self.sigma2,'a_z':self.a_z,'a_x':self.a_x,'dt':self.dt,'Y':self.Y}
         dmp_data = torch.tensor([self.Dof,self.N,self.time_steps,self.dt,self.a_x,self.a_z])

adding_DMP_layer_to_pretrained_network.py
@@ -5,6 +5,7 @@ from matLoader import matLoader
 from torch.autograd import Variable
 from trainer import Trainer
 from network import Network_DMP, training_parameters
+from os import makedirs
 
 from datetime import datetime
 N = 25
@@ -19,10 +20,20 @@ out = 2*N + 4
 
 layerSizes = [numOfInputs] + HiddenLayer + [out]
 
-net_id = '2018-04-25 13:36:32.095726'
+net_id = '2018-06-07 15:31:10.101849'
 
 directory_path = '/home/rpahic/Documents/Neural_networks/'
 directory_name = 'NN ' + net_id
+
+
+date = str(datetime.now())
+directory_name_new = 'NN ' + date
+parameters_file = directory_path + directory_name + '/net_parameters'
+makedirs(directory_path+directory_name_new)
+
+file = open(directory_path+directory_name+'/Network_description.txt','w')
+
+file.write('Network created: ' + date)
 
 model_old = torch.load(directory_path+directory_name+'/model.pt')
 parameters_file = directory_path + directory_name + '/net_parameters'
@@ -42,13 +53,13 @@ model.register_buffer('DMPp', model.DMPparam.data_tensor)
 model.register_buffer('scale_t', model.DMPparam.scale_tensor)
 model.register_buffer('param_grad', model.DMPparam.grad_tensor)
 
-input_image = Variable(torch.from_numpy(np.array(images[2]))).float()
-trajektorija=model(input_image)
+input_image = Variable(torch.from_numpy(np.array(images[2]))).float().view(1,-1)
+#trajektorija=model(input_image)
 trainer = Trainer()
 test_output = Variable(torch.from_numpy(np.array(outputs[2])), requires_grad=True).float()
 dmp = trainer.createDMP(test_output, scale, 0.01, 25, cuda=False)
 dmp.joint()
-mat = trainer.show_dmp(input_image.data.numpy(), trajektorija.data.numpy(), dmp, plot=True)
+#mat = trainer.show_dmp(input_image.data.numpy(), trajektorija.data.numpy(), dmp, plot=True)
 
 
 
@@ -74,18 +85,24 @@ train_param.val_fail = 60
 trener = Trainer()
 
 
-trener.indeks = np.load(directory_path + 'NN ' + net_id +'/net_indeks.npy')
+trener.indeks = np.load(directory_path + 'NN ' + net_id +'/net_indeks.npy')[0:1000]
 
-best_nn_parameters = trener.learn_DMP(model, images, original_trj, directory_path + directory_name, train_param, file, learning_rate, momentum)
+original_trj_e = []
+for i in range(0,images.shape[0]):
+    c,c1,c2 = zip(*original_trj[i])
+    original_trj_e.append(c)
+    original_trj_e.append(c1)
 
+best_nn_parameters = trener.learn_DMP(model, images[0:1000], original_trj_e[0:2000], directory_path + directory_name_new, train_param, file, learning_rate, momentum)
 
+original_trj[0].reshape(1,-1)
 
 
 #parameters = list(model.parameters())
 
 #torch.save(model.state_dict(), parameters_file) # saving parameters
 
-np.save(directory_path+directory_name+'/net_indeks', trener.indeks)
+np.save(directory_path+directory_name_new+'/net_indeks', trener.indeks)
 torch.save(best_nn_parameters, parameters_file)
 file.close()
 

test_DMP.py
@@ -55,6 +55,13 @@ dateset_name = 'slike_780.4251'
 
 images, outputs, scale, original_trj = matLoader.loadData(dateset_name, load_original_trajectories=True)
 net = Net(scale)
+original_trj_e = []
+for i in range(0,images.shape[0]):
+    c,c1,c2 = zip(*original_trj[i])
+    original_trj_e.append(c)
+    original_trj_e.append(c1)
+
+
 
 print(net)
 n=0
@@ -70,7 +77,7 @@ for p in list(net.parameters()):
 test_output = Variable(torch.from_numpy(np.array(outputs[40+n:42+n])), requires_grad=True).float()
 
 input_image = Variable(torch.from_numpy(np.array(images[40+n:42+n]))).float()
-
+traj=Variable(torch.from_numpy(np.array(original_trj_e[40+n:44+n]))).float()
 
 optimizer = torch.optim.Adam(net.parameters(), eps=0.001)
 optimizer.zero_grad()
@@ -97,7 +104,7 @@ net = net.cuda()
 #test_output1=test_output1.transpose(1,0)
 
 test_output = test_output.cuda()
-
+traj=traj.cuda()
 input_image = input_image.cuda()
 #trajektorija = DMP.forward(test_output)
 '''trajektorija = net(test_output)
@@ -109,7 +116,7 @@ loss_vector.backward()'''
 
 points = 10
 loss_graph = np.zeros((points, 3))
-n_w = 2
+n_w = 1
 start_w = test_output[0, n_w+1].item()
 print(start_w)
 
@@ -130,11 +137,11 @@ for j in range(0,points):
 
     to = torch.from_numpy(dmp.Y).float().transpose(1,0).cuda()
 
-    loss = criterion(trajektorija[0:2, :], to)
+    loss = criterion(trajektorija[:, :], traj)
 
     to = torch.cat((to,torch.from_numpy(dmp2.Y).float().transpose(1,0).cuda()))
 
-    loss_vector = criterion2(trajektorija[:, :], to)
+    loss_vector = criterion2(trajektorija[:, :], traj)
     '''f=torch.ones(2, 54).cuda()
     f.requires_grad=True
     r=DMP.apply(f, net.DMPp,net.param_grad,net.scale)
@@ -153,7 +160,7 @@ for j in range(0,points):
 
     loss_graph[j, 0] = test_output[0, n_w+1].item()
     loss_graph[j, 1] = loss_vector.item()
-    print(loss.item())
+    print(loss_vector.item())
 
     #loss_graph[j, 2] = net.param_grad[n_w, n_w].item()/test_output[0,n_w+1].item()
     #print(gradi[0][0,n_w+1])

testning_nn.py
@@ -8,7 +8,7 @@ import csv
 
 #Load model and parameters
 directory_path = '/home/rpahic/Documents/Neural_networks/'
-directory_name = 'NN ' + '2018-04-25 13:36:32.095726'
+directory_name = 'NN ' + '2018-06-07 15:31:10.101849'
 
 model = torch.load(directory_path+directory_name+'/model.pt')
 

trainer.py
@@ -313,7 +313,7 @@ class Trainer:
     def databaseSplit(self, images, outputs, train_set = 0.7, validation_set = 0.15, test_set = 0.15):
 
         r=len(images)
-
+        de =int(len(outputs)/r)
         trl=round(r*train_set)
         tel=round(r*test_set)
         val=r-trl-tel
@@ -338,14 +338,22 @@ class Trainer:
 
             if indeks[i] == 0:
                 x_t.append(images[i])
-                y_t.append(outputs[i])
+                y_t.append(outputs[i * de])
+
+                if de >1:
+                    y_t.append(outputs[i * de+1])
 
             if indeks[i] == 2:
                 x_v.append(images[i])
-                y_v.append(outputs[i])
+                y_v.append(outputs[i*de])
+                if de >1:
+                    y_v.append(outputs[i * de+1])
+
             if indeks[i] == 1:
                 x_te.append(images[i])
-                y_te.append(outputs[i])
+                y_te.append(outputs[i*de])
+                if de >1:
+                    y_te.append(outputs[i * de+1])
 
         x_train = np.array(x_t)
         y_train = np.array(y_t)
@@ -646,7 +654,7 @@ class Trainer:
         train_param.real_epochs = t
         train_param.min_train_loss = self.loss.data[0]
         train_param.min_val_loss = bestValLoss
-        train_param.min_test_loss = test_loss.data[0]
+        train_param.min_test_loss = test_loss
         train_param.elapsed_time = time_d.total_seconds()
         train_param.val_count = val_count
         k = (self.loss-oldLoss)/train_param.log_interval
@@ -754,7 +762,7 @@ class Trainer:
         # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 200)
 
         y_val = model(input_data_validate_b)
-        oldValLoss = criterion(y_val, output_data_validate_b[:, :]).data[0]
+        oldValLoss = criterion(y_val, output_data_validate_b[:, :])
         bestValLoss = oldValLoss
         best_nn_parameters = copy.deepcopy(model.state_dict())
         # Infiniti epochs
@@ -791,15 +799,21 @@ class Trainer:
             # writer.add_scalar('data/learning_rate', scheduler.get_lr()[0], t)
 
             self.loss = Variable(torch.Tensor([0]))
-            permutations = torch.randperm(len(input_data_train))
+            if t==1:
+                permutations = torch.randperm(len(input_data_train))
+                if model.isCuda():
+                    permutations = permutations.cuda()
+                    self.loss = self.loss.cuda()
             if model.isCuda():
-                permutations = permutations.cuda()
+
                 self.loss = self.loss.cuda()
             input_data_train = input_data_train[permutations]
-            output_data_train = output_data_train[permutations]
+            per = torch.stack([permutations,permutations+1]).transpose(1,0).contiguous().view(1,-1).squeeze()
+
+            output_data_train = output_data_train[per]
             ena = []
             while j <= len(input_data_train):
-                self.learn_one_step(model, input_data_train[i:j], output_data_train[i:j, :], learning_rate, criterion,
+                self.learn_one_step(model, input_data_train[i:j], output_data_train[i*2:j*2, :], learning_rate, criterion,
                                     optimizer)
                 i = j
                 j += train_param.bunch
@@ -813,7 +827,7 @@ class Trainer:
                             r1 = p.data[0][0]'''
 
             if i < len(input_data_train):
-                self.learn_one_step(model, input_data_train[i:], output_data_train[i:, :], learning_rate, criterion,
+                self.learn_one_step(model, input_data_train[i:], output_data_train[i*2:, :], learning_rate, criterion,
                                     optimizer)
 
             if (t - 1) % train_param.log_interval == 0:
@@ -834,23 +848,25 @@ class Trainer:
 
             if (t - 1) % train_param.validation_interval == 0:
                 y_val = model(input_data_validate)
-                val_loss = criterion(y_val, output_data_validate[:, 1:])
+
+                val_loss = criterion(y_val, output_data_validate[:, :])
+
                 writer.add_scalar('data/val_loss', math.log(val_loss), t)
-                if val_loss.data[0] < bestValLoss:
+                if val_loss < bestValLoss:
                     bestValLoss = val_loss.data[0]
                     best_nn_parameters = copy.deepcopy(model.state_dict())
                     saving_epochs = t
 
-                if val_loss.data[0] > bestValLoss:  # oldValLoss:
+                if val_loss > bestValLoss:  # oldValLoss:
                     val_count = val_count + 1
 
                 else:
 
                     val_count = 0
 
-                oldValLoss = val_loss.data[0]
+                oldValLoss = val_loss
                 writer.add_scalar('data/val_count', val_count, t)
-                print('Validatin: ', t, ' loss: ', val_loss.data[0], ' best loss:', bestValLoss)
+                print('Validatin: ', t, ' loss: ', val_loss, ' best loss:', bestValLoss)
 
                 if (t - 1) % 10 == 0:
                     state = model.state_dict()
@@ -874,13 +890,35 @@ class Trainer:
                     writer.add_scalars('data/var', var_dict, t)
 
                 if self.plot_im:
-                    plot_vector = torch.cat((output_data_validate[0, 0:1], y_val[0, :]), 0)
-                    dmp_v = self.createDMP(plot_vector, model.scale, 0.01, 25, True)
-                    dmp = self.createDMP(output_data_validate[0, :], model.scale, 0.01, 25, True)
-                    dmp.joint()
-                    dmp_v.joint()
-                    mat = self.show_dmp((input_data_validate.data[0]).cpu().numpy(), dmp.Y, dmp_v, plot=False)
-                    a = output_data_validate[0, :]
+
+                    fig = plt.figure()
+
+                    plt.imshow((np.reshape(input_data_validate.data[0].cpu().numpy(), (40, 40))), cmap='gray', extent=[0, 40, 40, 0])
+
+
+                    plt.plot(output_data_validate.data[0].cpu().numpy(), output_data_validate.data[1].cpu().numpy(), '--r', label='dmp')
+
+                    plt.plot(y_val.data[0].cpu().numpy(),y_val.data[1].cpu().numpy(), '-g', label='trajectory')
+                    plt.legend()
+                    plt.xlim([0, 40])
+                    plt.ylim([40, 0])
+
+                    fig.canvas.draw()
+                    matrix = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+
+
+
+                    mat= matrix.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+
+
+
+
+
+
+
+
+
                     writer.add_image('image' + str(t), mat)
                     self.plot_im = False
 
@@ -888,7 +926,7 @@ class Trainer:
 
             if (t - 1) % train_param.test_interval == 0:
                 y_test = model(input_data_test)
-                test_loss = criterion(y_test, output_data_test[:, 1:])
+                test_loss = criterion(y_test, output_data_test[:, :])
                 writer.add_scalar('data/test_loss', math.log(test_loss), t)
 
             '''if (t-1) % 1500 == 0: